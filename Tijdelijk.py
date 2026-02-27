5d436672a9960b61931afb1e134e25fd1d9444ff
veronedevries <veronedevries@gmail.com>
2026-02-27 15:35:56 +0100

    Dictionary toegevoegd

---
+++ Tijdelijk.py
@@ -0,0 +1,14 @@
+prijzen = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5,
+}
+
+a = 3
+v = 4 
+c = 5
+aanbieding = a * 0.8 
+
+reclame_tekst = (f"Vandaag in de aanbieiding: vanille-ijs, 1 liter - slechts €{aanbieding}")
+
+print(reclame_tekst)
