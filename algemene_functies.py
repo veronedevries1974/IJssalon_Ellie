cd7c3d1018cc2daa2823118123867a1a38c5c5e6
veronedevries1974 <veronedevries@gmail.com>
2026-03-04 11:29:31 +0100

    Add files via upload

---
+++ algemene_functies.py
@@ -0,0 +1,24 @@
+import math
+
+def mijn_functie_1(a=2, b=4, c=10, d=12):
+    return a**2, b**2, c**2, d**2
+totaal = mijn_functie_1()
+print(totaal)
+
+
+def mijn_functie_2(getallen_lijst):
+    resultaten = []
+    for getal in getallen_lijst:
+        geheel, decimaal = str(getal).split('.')
+        a, b = int(geheel), int(decimaal)
+        berekeningen = (f"[{a + b}, {a - b}, {a * b}, {a / b}]")
+        resultaten.append(str(berekeningen))
+        
+    return resultaten
+
+print(mijn_functie_2([12.3, 12.2, 10.5, 100.20]))
+
+ 
+        
+
+ 
