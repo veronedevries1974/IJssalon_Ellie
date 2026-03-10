8b7f7087a45f556d542aa8102a4094abc435a0db
veronedevries1974 <veronedevries@gmail.com>
2026-03-10 12:20:56 +0100

    dict. inkomsten, import helper, totale som

---
+++ boekhouding.py
@@ -0,0 +1,12 @@
+from helper import *
+
+inkomsten = {
+    "Aardbeien-ijs-totaal" : 1000,
+    "Vanille-ijs-totaal" : 2000,
+    "Chocolade-ijs-totaal" : 1500,
+    "Waterijsjes-totaal" : 750    
+}
+
+totaal_inkomsten = som(inkomsten)
+
+print(totaal_inkomsten)
