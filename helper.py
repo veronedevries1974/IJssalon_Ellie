5b377f444ecd6104c9497f698500fe1a3fbaf0f2
veronedevries1974 <veronedevries@gmail.com>
2026-03-10 12:21:35 +0100

    functie som toegevoegd

--- helper.py
+++ helper.py
@@ -1,7 +1,23 @@
-tekst="header"
-lengte = len(tekst) + 4
-print()
-print(lengte * "*")
-print(f"*{tekst}*")
-print(lengte *"*")
-print()
+def decoreer(tekst=""):
+    tekst="header"
+    lengte = len(tekst) + 4
+    print()
+    print(lengte * "*")
+    print(f"*{tekst}*")
+    print(lengte *"*")
+    print()
+    
+def fooi_pp(bedrag, personen):
+    bedrag_pp = bedrag / personen
+    return f"Het bedrag per persoon is {bedrag_pp} euro."
+
+def onderstreep(tekst=""):
+    uit = []
+    uit.append(tekst)
+    uit.append(len(tekst)*"=")
+    return uit
+
+def som(inkomsten):
+    resultaat = sum(inkomsten.values())
+    return resultaat
+        
